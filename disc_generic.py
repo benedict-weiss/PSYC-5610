73b63333ceab7f2e13e7a443892138ded742a1a5
Ben Weiss <psyc2610_bcw45@a1132u37n01.mghpcc.ycrc.yale.edu>
2025-11-26 19:38:39 -0500

    kept working on generative model

--- disc_generic.py
+++ disc_generic.py
@@ -8,11 +8,14 @@ def initialise_scene(radius,
                     light_elevation,
                     light_rotation):
 
+    # light elevation: 0 is grazing from side, -pi/2 is from below, pi/2 is from above
+    # light rotation: tbc
+
     disc_elevation_deg = float(np.rad2deg(disc_elevation)) # necessary conversion?
     disc_rotation_deg = float(np.rad2deg(disc_rotation))
 
     disc_to_world = (
-        T.translate([0.0, 0.0, 0.0])
+        T.translate([0.0, 0.0, 1.0])
         .rotate([0, 1, 0], disc_rotation_deg)
         .rotate([1, 0, 0], disc_elevation_deg)
         .scale(radius)
@@ -21,15 +24,14 @@ def initialise_scene(radius,
     l_x = float(np.cos(light_elevation) * np.cos(light_rotation))
     l_y = float(np.sin(light_elevation))
     l_z = float(np.cos(light_elevation) * np.sin(light_rotation))
-    # might need to invert this?
+    # inversion here?
     light_direction = [l_x, l_y, l_z]
 
-    # copied from cbox-generic
+    # inspired by cbox-generic
     
     d = {
-        # inconsistent quotations?
-        "type": "scene",
-        "integrator": {
+        'type': 'scene',
+        'integrator': {
             'type': 'path',
             'max_depth': 6,
         },
@@ -48,14 +50,14 @@ def initialise_scene(radius,
             ),
             'sampler': {
                 'type': 'independent',
-                'sample_count': 16
+                'sample_count': 64 # bumped this
             },
             'film': {
                 'type': 'hdrfilm',
                 'width' : 256,
                 'height': 256,
                 'rfilter': {
-                    'type': 'tent',
+                    'type': 'tent', # or box here?
                 },
                 'pixel_format': 'luminance', # changed here
                 'component_format': 'float32',
@@ -64,14 +66,16 @@ def initialise_scene(radius,
 
         'gray': {
             'type': 'diffuse',
-            'reflectance': 0.85
+            'reflectance': 0.5
         },
 
-        # good intensity here?
-        'light': {
-            'type': 'directional',
-            'direction': light_direction,
-            'irradiance': 1.0
+        'background': {
+            'type': 'rectangle',
+            'to_world': T.translate([0.0, 0.0, 0.0]).scale(3.0), # or 4.0 maybe?
+            'bsdf': {
+                'type': 'ref',
+                'id': 'gray'
+            }
         },
 
         # not great notation here haha
@@ -82,7 +86,16 @@ def initialise_scene(radius,
                 'type': 'ref',
                 'id': 'gray'
             }
-        }
+        },
+
+        # good intensity here?
+        'light': {
+            'type': 'directional',
+            'direction': light_direction,
+            'irradiance': 2.0
+        },
+
+
     }
 
     return d
